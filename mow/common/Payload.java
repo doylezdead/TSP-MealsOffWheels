a0be4c98fc5ec499caee2799867ecaf4a26430a5
Ryan Doyle <rcdoyle@mtu.edu>
2015-03-18 16:30:26 -0400

    removed old maps api stuff and added an outline to the runner

--- mow/common/Payload.java
+++ mow/common/Payload.java
@@ -4,10 +4,10 @@ import java.io.Serializable;
 public class Payload implements Serializable{
 	public int value; //return code for non-coordinate returning values. can also define send or return.
 	public int opcode; //represent different operations or requests. 
+	public int weight;
 	public double xcoord; //gps locations
 	public double ycoord; //gps locations
 	public String name; //name of orderee. for database storage.
-	public int order_num;
 	public String password;
 
 	public Payload(int value, double xcoord, double ycoord, String name){
--- mow/server/Runner.java
+++ mow/server/Runner.java
@@ -2,6 +2,7 @@ package mow.server;
 
 import mow.common.Payload;
 
+
 class Runner{
 	/**
 	 * checkSecurity returns true on a success
@@ -14,25 +15,97 @@ class Runner{
 	
 	public Payload parseOps(Payload readData){
 		
-		Payload retData = null;
+		Payload retData = new Payload(0,0,0,"fail");
 		
-		if(readData.value != 0){
+		if(readData.value == -1){
 			throw Exception("parseOps failed with payload value of " + readData.value);
 		}
-
-		int opcode = readData.opcode;
 		
+		int value = readData.value;
+		int opcode = readData.opcode;
+		int weight = readData.weight;
 		double xcoord = readData.xcoord;
 		double ycoord = readData.ycoord;
 		String name = readData.name;
 		int order_num = null; //this will be defined and returned in the return payload
 		
+		switch(opcode){
+			case 0: // checking deliverability
+					int storeID = findNearestStore(xcoord,ycoord);
+					if (checkStoreStatus(storeID)){
+						retData.value = storeID;
+						retData.opcode = 0;
+					}
+			case 1:	// Placing an order
+					retData.value = placeOrder(name, 15174047199, value, xcoord, ycoord, weight);
+					
+			case 2:	// Retrieving order status
+					if (checkOrderIsGood(value)){
+						retData.value = checkOrderETA(value);
+						retData.xcoord = getOrderXPos(value);
+						retData.ycoord = getOrderYPos(value);
+					}
+		}
+		
 
-
-
-		return readData;
+		//RETURNING A ZERO VALUE TO THE CLIENT MEANS SOMETHING FAILED. MUST CHECK ON CLIENT
+		return retData;
 	}
 	private
 
 
 }
+
+// Store status. checking deliverability
+private int findNearestStore(double x, double y){
+	return 0; //return nearest store's id
+}
+
+private boolean checkStoreStatus(){
+	return true; // or false
+}
+//
+
+// Place Order
+private int placeOrder(String name, int phone, int storeID, double x, double y, int weight){
+	//create order. assign a drone id and a store id
+	orderID = 0;
+	//orderID = new Order();
+	
+	return orderID; //returns newly created order identity
+}
+//
+
+//Getting Order Status
+private int checkOrderETA(int OrderID){
+	return 0; //return time in seconds
+}
+
+private double getOrderXPos(int OrderID){
+	return 0;
+}
+
+private double getOrderYPos(int OrderID){
+	return 0;
+}
+
+private boolean checkOrderIsGood(int OrderID){
+	return true: //or false. true is good status
+}
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
