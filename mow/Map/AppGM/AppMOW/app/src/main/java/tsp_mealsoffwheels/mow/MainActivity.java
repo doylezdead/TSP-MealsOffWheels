73aaecac1b49a11be8de63de8179b46c3fbeeadc
Yorkshyre <dcmohrha@mtu.edu>
2015-03-27 12:54:31 -0400

    Smooth Travel Animation
    I added a few classes to handle smooth travel animation on the maps
    activity end...

--- mow/Map/AppGM/AppMOW/app/src/main/java/tsp_mealsoffwheels/mow/MainActivity.java
+++ mow/Map/AppGM/AppMOW/app/src/main/java/tsp_mealsoffwheels/mow/MainActivity.java
@@ -1,25 +1,43 @@
 package tsp_mealsoffwheels.mow;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdate;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.GoogleMap.OnMapLoadedCallback.*;
+
+import static tsp_mealsoffwheels.mow.MarkerAnimation.*;
 
 public class MainActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    //private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+    private PolylineOptions path;
+    private LatLng droneHome = new LatLng(47.11240, -88.58705);
+    private LatLng thisHome = new LatLng(47.1206714, -88.5545586);
+    private Marker mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         setUpMapIfNeeded();
     }
 
@@ -65,7 +83,58 @@ public class MainActivity extends FragmentActivity {
      */
     private void setUpMap() {
         // Just adding a generic marker
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(47.1206714, -88.5545586), 14.0f));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(47.1206714, -88.5545586)).title("Drone Home"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(47.1206714, -88.5545586), 12.0f)
+        );
+
+        mMap.addMarker(new MarkerOptions().position(
+                        thisHome).title("Dave's Home")
+        );
+
+        mMap.addMarker(new MarkerOptions().position(droneHome).title("Taco Bell!!!"));
+        mark = mMap.addMarker(new MarkerOptions().position(droneHome).title("Drone"));
+
+        path = new PolylineOptions().geodesic(true)
+                .add(droneHome)                             //  Taco Bell on Razorback
+                .add(thisHome)
+                .color(Color.argb(1000,128,0,128));
+
+        mMap.addPolyline(path);
+
+        Log.d("BEARING", "should be 069.28 and is " + getBearing(droneHome, thisHome));
+        Log.d("DISTANCE", "The distance between two points on the map is");
+        //LocationManager locMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                if(mMap != null) {
+                    animateMarkerAtoB(mark, thisHome, new LatLngInterpolator.Spherical());
+                }
+            }
+        });
+    }
+
+    /**
+     * Written by David Mohrhardt
+     * @param from
+     * @param to
+     * @return
+     */
+    private double getBearing(LatLng from, LatLng to) {
+        double theta;
+
+        double startLat = Math.toRadians( from.latitude );
+        double startLon = Math.toRadians( from.longitude);
+        double endLat = Math.toRadians( to.latitude );
+        double endLon = Math.toRadians( to.longitude );
+
+        double y = Math.sin(endLon - startLon) * Math.cos(endLat);
+        double x = Math.cos(startLat) * Math.sin(endLat) -
+                Math.sin(startLat) * Math.cos(endLat) * Math.cos(endLon - startLon);
+
+        theta = Math.atan2(y, x);
+        theta = Math.toDegrees( theta );
+
+        return theta;
     }
 }
