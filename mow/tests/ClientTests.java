165613acae10bde885169ee9097195b76239d45f
Ryan Doyle <rcdoyle@mtu.edu>
2015-04-07 21:33:55 -0400

    fixed packaging for JUnit tests

--- mow/tests/ClientTests.java
+++ mow/tests/ClientTests.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 import mow.common.Payload;
 
-public class ClientTest {
+public class ClientTests {
 
 	@Test
 	public void testClientToServer(){
--- mow/tests/TestRunner.java
+++ mow/tests/TestRunner.java
@@ -1,3 +1,5 @@
+package mow.tests;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
