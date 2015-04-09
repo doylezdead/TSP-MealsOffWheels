f444b9ca1b8bf6f53c6ac12ec5b9cc75b7eab639
Yorkshyre <dcmohrha@mtu.edu>
2015-04-09 19:59:29 -0400

    Made some changes to the DroneFinder
    It should be noted that the activity does have a constructor such that
    you can pull information from the application in other portions of the
    app.

--- DroneDelivery/app/src/main/java/com/mealsoffwheels/dronedelivery/activities/DroneFinder.java
+++ DroneDelivery/app/src/main/java/com/mealsoffwheels/dronedelivery/activities/DroneFinder.java
@@ -24,6 +24,7 @@ import com.mealsoffwheels.dronedelivery.map.LatLngInterpolator;
 import static com.mealsoffwheels.dronedelivery.map.MarkerAnimation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -32,10 +33,21 @@ public class DroneFinder extends FragmentActivity {
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private PolylineOptions path;
-    private LatLng droneHome = new LatLng(47.11240, -88.58705);
+    private List<LatLng> latlngPath;
+    private LatLng droneHome = new LatLng(47.11240, -88.58705);     // Taco Bell at Razorback Dr.
     private LatLng thisHome;
-    private LatLng deviceLocation;
+    //private LatLng deviceLocation;
     private Marker mark;
+
+    public DroneFinder(String dest, boolean destinationFlag) {
+        if(destinationFlag = true) {
+            thisHome = getLatLngFromAddress(dest, getApplicationContext());
+        }
+        else {
+            thisHome = getDeviceLocation();
+        }
+        setUpMapIfNeeded();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,20 +97,17 @@ public class DroneFinder extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        // Just adding a generic marker
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                         droneHome, 12.0f)
         );
 
-        thisHome = getLatLngFromAddress("1304 College Avenue, Houghton MI 49931", getApplicationContext());
-
         mMap.addMarker(new MarkerOptions().position(
-                        thisHome).title("Dave's Home")
+                        thisHome).title("Destination")
         );
 
-        deviceLocation = getDeviceLocation();
+        //deviceLocation = getDeviceLocation();
 
-        mMap.addMarker(new MarkerOptions().position(droneHome).title("Taco Bell!!!"));
+        mMap.addMarker(new MarkerOptions().position(droneHome).title("Taco Bell"));
         mark = mMap.addMarker(new MarkerOptions().position(droneHome).title("Drone"));
 
         path = new PolylineOptions().geodesic(true)
@@ -107,10 +116,7 @@ public class DroneFinder extends FragmentActivity {
                 .color(Color.argb(1000, 128, 0, 128));
 
         mMap.addPolyline(path);
-
-        //Log.d("BEARING", "should be 069.28 and is " + getBearing(droneHome, thisHome) + " so error = " + (getBearing(droneHome, thisHome) - 069.28));
-        Log.d("DISTANCE", "The distance between two points on the map is");
-        getLatLngFromAddress("4820 Westchester Commons, Traverse City MI 49684", getApplicationContext());
+        latlngPath = path.getPoints();
 
         //LocationManager locMan = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
@@ -195,11 +201,20 @@ public class DroneFinder extends FragmentActivity {
             Log.d("CUR_LAT", "" + myLocation.getLatitude());
             Log.d("CUR_LNG", "" + myLocation.getLongitude());
             curLoc = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());    // Create a LatLng object of the devices current location.
-            mMap.addMarker(new MarkerOptions().position(curLoc).title("Device Location"));
         }
         else
             Log.d("DEVICE_LOCATION_FAILURE", "Failed to retrieve the device location");
         // End device location block
         return curLoc;
+    }
+
+    /**
+     * Get the LatLng path from point A to point B on the map and pass that back to the main
+     * program.
+     *
+     * @return latlngPath The list of LatLng points that makes up the shortest distance path.
+     */
+    public List<LatLng> getLatLngPath() {
+        return latlngPath;
     }
 }
